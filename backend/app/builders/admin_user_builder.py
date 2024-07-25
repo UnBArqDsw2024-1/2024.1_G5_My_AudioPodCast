b53eae81d8af83c9fdb141dc6a4d67aabe4f4c68
meanmustard <caio.braga.felipe@gmail.com>
2024-07-24 23:00:44 -0300

    feat: adiciona strategies do backend para a camada de permissions
    Co-authored-by: caio-lelis <cayolelisdf@gmail.com>

--- backend/app/builders/admin_user_builder.py
+++ backend/app/builders/admin_user_builder.py
@@ -1,7 +1,8 @@
 from backend.app.enum.user_permission_enum import UserPermission
 from backend.app.builders.user_builder import UserBuilder
+from backend.app.strategies.admin_user_permission_strategy import AdminPermissionStrategy
 
 class AdminUserBuilder(UserBuilder):
       def build(self):
-        self.user.permission = UserPermission.ADMIN
+        self.user.permission = AdminPermissionStrategy()
         return self.user
