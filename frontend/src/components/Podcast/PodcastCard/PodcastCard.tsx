f767c34fa9d26a890dca512e6a1746a9d72438db
DelzironBraz <delziron.braz@hotmail.com>
2024-08-06 12:12:28 -0300

    feat(podcast section)

---
+++ frontend/src/components/Podcast/PodcastCard/PodcastCard.tsx
@@ -0,0 +1,26 @@
+import { BsSoundwave } from "react-icons/bs";
+import { IPodcast } from "../../../utils/podcast-helper";
+
+const PodcastCard = (podcast: IPodcast) => {
+  return (
+    <div className="w-[320px] h-[500px] border border-gray-300 border-solid rounded-md flex flex-col justify-start items-center gap-5 shadow-md transform transition-transform duration-300 hover:scale-105 cursor-pointer">
+        <img
+            className="w-full h-1/2 object-cover" 
+            src={podcast.img.src} 
+            alt={podcast.img.alt} 
+        />
+
+        <div className="flex flex-col justify-start items-start gap-5 p-5">
+            <h3 className="text-black font-semibold text-xl">{podcast.title}</h3>
+            <h6 className="text-black font-semibold">{podcast.author}</h6>
+            <p className="text-black text-sm font-medium text-start line-clamp-3">{podcast.description}</p>
+            <button className="border-blue-600 border hover:bg-blue-600 p-2 font-bold text-blue-600 hover:text-white rounded-md flex justify-center items-center gap-2">
+              <BsSoundwave />
+              Ouça agora
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default PodcastCard
---
+++ frontend/src/components/Podcast/PodcastSection/PodcastSection.tsx
@@ -0,0 +1,24 @@
+import { Link } from '../../../../node_modules/react-router-dom/dist/index'
+import PodcastGrid from '../PodcastGrid/PodcastGrid'
+
+
+const PodcastSection = () => {
+  return (
+    <section id="podcast" className="w-screen h-full px-5">
+        <div className="w-full flex flex-col justify-center items-center gap-3 p-5">
+            <h3 className="text-black font-bold text-4xl">Podcasts</h3>
+            <p className="text-medium text-xl text-center">
+                Ouça por aqui os podcast mais recentes
+            </p>
+        </div>
+
+        <PodcastGrid />
+
+        <div className="w-full py-5 flex justify-center items-center">
+            <Link to="/podcast" className="bg-blue-600 hover:bg-blue-700 py-4 px-5 font-bold text-white rounded-md w-[200px] text-center">Ver mais</Link> 
+        </div>
+    </section>
+  )
+}
+
+export default PodcastSection
