95571f65eec2ead583de9bdec911d81b3adaa653
DelzironBraz <delziron.braz@hotmail.com>
2024-08-06 11:49:00 -0300

    feat(config redux/react query/react router/tailwind/global css)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,28 @@
+import './index.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { BrowserRouter } from '../node_modules/react-router-dom/dist/index'
+import { Provider } from 'react-redux'
+
 import App from './App.tsx'
-import './index.css'
+import store from './utils/redux/store';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App /> 
+        </BrowserRouter>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
