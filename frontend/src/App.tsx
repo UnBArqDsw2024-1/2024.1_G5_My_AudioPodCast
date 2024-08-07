5836e32f62d44afbdf1236a9bc6c5e7e777a51e8
AnaBeatrizMassuh <aaamassuh@gmail.com>
2024-08-06 22:56:57 -0300

    Implementação da  página de login com campos de entrada para email e senha, funcionalidade de mostrar/ocultar senha e roteamento para a página de cadastro

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from '../node_modules/react-router-dom/dist/index'
 import Home from './pages/Home/Home'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
+import SignIn from './pages/SignIn/SignIn'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route  path="/" element={<Home />}/>
+        <Route path="/sign-in" element={<SignIn />} />
       </Routes>
       <Footer />
     </>
