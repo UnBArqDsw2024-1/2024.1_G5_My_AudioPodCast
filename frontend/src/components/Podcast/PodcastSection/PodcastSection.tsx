d39129c256383365443016ea05895317142fe5be
EmersonTeles <emersonteles21@gmail.com>
2024-08-07 17:32:37 -0300

    fix: home width overflow

--- frontend/src/components/Podcast/PodcastSection/PodcastSection.tsx
+++ frontend/src/components/Podcast/PodcastSection/PodcastSection.tsx
@@ -1,24 +1,26 @@
-import { Link } from '../../../../node_modules/react-router-dom/dist/index'
-import PodcastGrid from '../PodcastGrid/PodcastGrid'
-
+import { Link } from "../../../../node_modules/react-router-dom/dist/index";
+import PodcastGrid from "../PodcastGrid/PodcastGrid";
 
 const PodcastSection = () => {
   return (
-    <section id="podcast" className="w-screen h-full px-5">
-        <div className="w-full flex flex-col justify-center items-center gap-3 p-5">
-            <h3 className="text-black font-bold text-4xl">Podcasts</h3>
-            <p className="text-medium text-xl text-center">
-                Ouça por aqui os podcast mais recentes
-            </p>
-        </div>
+    <section id="podcast" className="w-full h-full px-5">
+      <div className="w-full flex flex-col justify-center items-center gap-3 p-5">
+        <h3 className="text-black font-bold text-4xl">Podcasts</h3>
+        <p className="text-medium text-xl text-center">Ouça por aqui os podcast mais recentes</p>
+      </div>
 
-        <PodcastGrid />
+      <PodcastGrid />
 
-        <div className="w-full py-5 flex justify-center items-center">
-            <Link to="/podcast" className="bg-blue-600 hover:bg-blue-700 py-4 px-5 font-bold text-white rounded-md w-[200px] text-center">Ver mais</Link> 
-        </div>
+      <div className="w-full py-5 flex justify-center items-center">
+        <Link
+          to="/podcast"
+          className="bg-blue-600 hover:bg-blue-700 py-4 px-5 font-bold text-white rounded-md w-[200px] text-center"
+        >
+          Ver mais
+        </Link>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default PodcastSection
+export default PodcastSection;
