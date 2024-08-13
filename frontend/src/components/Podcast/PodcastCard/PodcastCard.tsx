7672430def3be012b48c5b4de26e52f89445bef0
SamuelCastro7 <samuelcastro2010@gmail.com>
2024-08-12 22:12:48 -0300

    Criando a reprodução dos podcasts
    Co-authored-by: milenaaires <mbasdias@gmail.com>

--- frontend/src/components/Podcast/PodcastCard/PodcastCard.tsx
+++ frontend/src/components/Podcast/PodcastCard/PodcastCard.tsx
@@ -3,24 +3,31 @@ import { IPodcast } from "../../../utils/podcast-helper";
 
 const PodcastCard = (podcast: IPodcast) => {
   return (
-    <div className="w-[320px] h-[500px] border border-gray-300 border-solid rounded-md flex flex-col justify-start items-center gap-5 shadow-md transform transition-transform duration-300 hover:scale-105 cursor-pointer">
-        <img
-            className="w-full h-1/2 object-cover" 
-            src={podcast.img.src} 
-            alt={podcast.img.alt} 
-        />
+    <div className="w-[320px] h-[600px] border border-gray-300 border-solid rounded-md flex flex-col justify-start items-center gap-5 shadow-md transform transition-transform duration-300 hover:scale-105 cursor-pointer">
+      <img
+        className="w-full h-1/2 object-cover"
+        src={podcast.img.src}
+        alt={podcast.img.alt}
+      />
 
-        <div className="flex flex-col justify-start items-start gap-5 p-5">
-            <h3 className="text-black font-semibold text-xl">{podcast.title}</h3>
-            <h6 className="text-black font-semibold">{podcast.author}</h6>
-            <p className="text-black text-sm font-medium text-start line-clamp-3">{podcast.description}</p>
-            <button className="border-blue-600 border hover:bg-blue-600 p-2 font-bold text-blue-600 hover:text-white rounded-md flex justify-center items-center gap-2">
-              <BsSoundwave />
-              Ouça agora
-            </button>
-        </div>
+      <div className="flex flex-col justify-start items-start gap-5 p-5">
+        <h3 className="text-black font-semibold text-xl">{podcast.title}</h3>
+        <h6 className="text-black font-semibold">{podcast.author}</h6>
+        <p className="text-black text-sm font-medium text-start line-clamp-3">{podcast.description}</p>
+        <button className="border-blue-600 border hover:bg-blue-600 p-2 font-bold text-blue-600 hover:text-white rounded-md flex justify-center items-center gap-2">
+          <BsSoundwave />
+          Ouça agora
+        </button>
+        <iframe
+          src={podcast.audioSrc}
+          width="100%"
+          height="80" // Ajusta a altura do iframe conforme necessário
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PodcastCard
+export default PodcastCard;
--- frontend/src/components/Podcast/PodcastGrid/PodcastGrid.tsx
+++ frontend/src/components/Podcast/PodcastGrid/PodcastGrid.tsx
@@ -1,16 +1,17 @@
-import { IPodcast, podcasts } from "../../../utils/podcast-helper"
-import PodcastCard from "../PodcastCard/PodcastCard"
+import { IPodcast, podcasts } from "../../../utils/podcast-helper";
+import PodcastCard from "../PodcastCard/PodcastCard";
 
 const PodcastGrid = () => {
   return (
     <div className="w-full max-w-[1250px] mx-auto">
-        <div className="w-full flex flex-wrap justify-center lg:justify-between items-center gap-10">
-            {podcasts.map((podcast: IPodcast, index: number) => (
-                <PodcastCard key={index} {...podcast} />
-            ))}
-        </div>
+      <div className="w-full flex flex-wrap justify-center lg:justify-between items-center gap-10">
+        {podcasts.map((podcast: IPodcast, index: number) => (
+          // Passando o objeto `podcast` diretamente para o `PodcastCard`
+          <PodcastCard key={index} {...podcast} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PodcastGrid
+export default PodcastGrid;
