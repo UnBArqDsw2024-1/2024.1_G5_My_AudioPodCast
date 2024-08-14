8852f13fbcb960f23dff0a8e279f399f58a1c82e
EmersonTeles <emersonteles21@gmail.com>
2024-08-14 00:15:42 -0300

    feat: add show banner to page

--- frontend/src/pages/Show/Show.tsx
+++ frontend/src/pages/Show/Show.tsx
@@ -1,5 +1,11 @@
+import ShowBanner from "../../components/ShowBanner/ShowBanner";
+
 const Show = () => {
-  return <main className="w-full h-full p-10 mt-20"></main>;
+  return (
+    <main className="w-full h-full p-10 mt-20">
+      <ShowBanner />
+    </main>
+  );
 };
 
 export default Show;
