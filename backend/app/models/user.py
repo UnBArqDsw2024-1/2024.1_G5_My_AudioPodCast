766bf602db1356b693dcb6b3b8c453694f40b38b
caioalvesbraga <caioalvesbraga@Caio-Compiuter>
2024-08-14 14:30:35 -0300

    fix: retira o strategy da models de User

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, Integer, String, Enum as SqlEnum
 from sqlalchemy.orm import relationship
 from app.enum.user_permission_enum import UserPermission
 from app.database import Base
-from app.strategies.user_permission_strategy import UserPermissionStrategy
 
 class User(Base):
     __tablename__ = "users"
@@ -16,11 +15,3 @@ class User(Base):
     
     playlists = relationship("Playlist", back_populates="owner")
     ratings = relationship("Rating", back_populates="user")
-
-    def set_permission_strategy(self, strategy: UserPermissionStrategy):
-        self.permission_strategy = strategy
-
-    def access_resources(self):
-        if self.permission_strategy:
-            return self.permission_strategy.access_resources()
-        return "Permissão não definida."
