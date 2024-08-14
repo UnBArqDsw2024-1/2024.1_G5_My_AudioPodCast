70a92848b861e670dcda78be069461af8c72eaad
ArtAssLou <dassumpcaoarthur@gmail.com>
2024-08-14 01:17:48 -0300

    adicionando episodios na lista do podcast

--- frontend/src/components/Episode/EpisodeSection/EpisodeSection.tsx
+++ frontend/src/components/Episode/EpisodeSection/EpisodeSection.tsx
@@ -3,20 +3,18 @@ import EpisodeGrid from "../EpisodeGrid/EpisodeGrid";
 
 const EpisodeSection = () => {
   return (
-    <section id="episode" className="w-full h-full px-5">
-      <div className="w-full flex flex-col justify-center items-center gap-3 p-5">
-        <h3 className="text-black font-bold text-4xl">Artigos</h3>
-        <p className="text-medium text-xl text-center">Veja os últimos artigos postados</p>
+    <section id="episodes" className="w-full h-full max-w-screen-xl mx-auto">
+      <div className="w-full flex flex-col justify-start items-start gap-3 p-5">
+        <h3 className="text-black font-bold text-4xl">Todos Episódios</h3>
+        <p className="text-medium text-xl text-center">Veja os últimos episódios postados</p>
       </div>
-
       <EpisodeGrid />
-
-      <div className="w-full py-5 flex justify-center items-center">
+      <div className="w-full py-8 flex justify-center items-center">
         <Link
-          to="/articles"
-          className="bg-blue-600 hover:bg-blue-700 py-4 px-5 font-bold text-white rounded-md w-[200px] text-center"
+          to="#episodes"
+          className="bg-blue-600 hover:bg-blue-800 py-4 px-5 font-bold text-white rounded-md w-[200px] text-center"
         >
-          Ver mais
+          Ver mais episodios
         </Link>
       </div>
     </section>
--- frontend/src/pages/Show/Show.tsx
+++ frontend/src/pages/Show/Show.tsx
@@ -1,9 +1,11 @@
 import ShowBanner from "../../components/ShowBanner/ShowBanner";
+import EpisodeSection from "../../components/Episode/EpisodeSection/EpisodeSection";
 
 const Show = () => {
   return (
     <main className="w-full h-full">
       <ShowBanner />
+      <EpisodeSection />
     </main>
   );
 };
