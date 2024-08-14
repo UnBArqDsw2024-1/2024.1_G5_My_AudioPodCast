248e7d23ceac442599a250fa3a568465be812629
EmersonTeles <emersonteles21@gmail.com>
2024-08-14 00:13:03 -0300

    feat: add new show route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,24 @@
-import './App.css'
+import "./App.css";
 
-import { Route, Routes } from '../node_modules/react-router-dom/dist/index'
-import Home from './pages/Home/Home'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
-import SignIn from './pages/SignIn/SignIn'
+import { Route, Routes } from "../node_modules/react-router-dom/dist/index";
+import Home from "./pages/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import SignIn from "./pages/SignIn/SignIn";
+import Show from "./pages/Show/Show";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route  path="/" element={<Home />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/show/:showName" element={<Show />} />
         <Route path="/sign-in" element={<SignIn />} />
       </Routes>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
