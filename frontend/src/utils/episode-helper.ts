531b6f6533d60d7de8059476561d6e829ec4dbb7
Arthur D'Assumpcao <dassumpcaoarthur@gmail.com>
2024-08-14 23:24:30 -0300

    feat: populate episodes

--- frontend/src/utils/episode-helper.ts
+++ frontend/src/utils/episode-helper.ts
@@ -15,64 +15,61 @@ export const episodes: IEpisode[] = [
       alt: "article 01",
       src: "/article/article_01.jpg",
     },
-    title: "The Rise of AI in Daily Life",
-    author: "Sophia Turner",
+    title: "IA e o nosso cotidiano",
+    author: "Casos e Causos",
     create_at: "05/12/2023",
     description:
-      "This article explores how artificial intelligence is becoming an integral part of our everyday lives.",
+      "Ete artigo explora como a inteligencia artifical tem mudado o cotidiano das pessoas",
   },
   {
     img: {
       alt: "article 02",
       src: "/article/article_02.jpg",
     },
-    title: "Exploring the Deep Ocean",
-    author: "James Cameron",
+    title: "Adoção Pet e saúde mental",
+    author: "Casos e Causos",
     create_at: "14/07/2022",
     description:
-      "An in-depth look at the latest discoveries from the uncharted territories of the deep ocean.",
+      "Este é um relato de como a adoção de Pets pode ajudar em tratamentos de transtornos mentais.",
   },
   {
     img: {
       alt: "article 03",
       src: "/article/article_03.jpg",
     },
-    title: "Sustainable Living: A Guide",
-    author: "Emily Clarke",
+    title: "Moda e Sustentabilidade",
+    author: "Casos e Causos",
     create_at: "22/11/2021",
-    description: "A comprehensive guide to adopting a more sustainable and eco-friendly lifestyle.",
+    description: "A moda vem sendo uma das industrias mais poluentes do mundo, mas não precisa ser assim.",
   },
   {
     img: {
       alt: "article 04",
       src: "/article/article_04.jpg",
     },
-    title: "The Future of Space Travel",
-    author: "Michael Johnson",
+    title: "Agricultura familiar",
+    author: "Casos e Causos",
     create_at: "30/04/2023",
-    description:
-      "An article discussing the advancements in space travel and what the future might hold for humanity beyond Earth.",
+    description: "O impacto da agricultura familiar na mesa dos Brasileiros",
   },
   {
     img: {
       alt: "article 05",
       src: "/article/article_05.jpg",
     },
-    title: "The Impact of Social Media",
-    author: "Olivia Green",
+    title: "Reciclagem em Casa",
+    author: "Casos e Causos",
     create_at: "17/08/2022",
-    description:
-      "An analysis of how social media is shaping our society and influencing personal relationships.",
+    description: "Reciclar em casa pode gerar economia e ajuda o meio ambiente",
   },
   {
     img: {
       alt: "article 06",
-      src: "/article/article_06.jpg",
-    },
-    title: "Understanding Quantum Computing",
-    author: "David Lee",
+      src: "/article/article_06.jpg"
+   },
+    title: "Mulheres na ciencia",
+    author: "Casos e Causos",
     create_at: "09/01/2024",
-    description:
-      "An introductory article explaining the basics of quantum computing and its potential applications.",
+    description: "Uma historia sobre as maiores cientistas que o mundo já teve.",
   },
 ];
