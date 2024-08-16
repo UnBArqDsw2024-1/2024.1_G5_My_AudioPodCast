b62e6c0059adb2117d8fb0a40f06cc4db59ee743
Delziron Lima <delziron.braz@hotmail.com>
2024-08-15 22:42:45 -0300

    refactor(make easier to integrate)

--- frontend/src/components/Article/ArticleSection/ArticleSection.tsx
+++ frontend/src/components/Article/ArticleSection/ArticleSection.tsx
@@ -1,7 +1,19 @@
-import { Link } from "../../../../node_modules/react-router-dom/dist/index";
+import { useEffect, useState } from "react";
 import ArticleGrid from "../ArticleGrid/ArticleGrid";
+import { articles, IArticle } from "../../../utils/article-helper";
 
 const ArticleSection = () => {
+  const [articlesData, setArticles] = useState<IArticle[]>([]);
+  const [seeMore, setSeeMore] = useState<number>(4);
+
+  const handleSeeMore = () => {
+    setSeeMore(seeMore + 2);
+  };
+
+  useEffect(() => {
+    setArticles(articles);
+  }, [articlesData])
+
   return (
     <section id="article" className="w-full h-full px-5">
       <div className="w-full flex flex-col justify-center items-center gap-3 p-5">
@@ -9,15 +21,17 @@ const ArticleSection = () => {
         <p className="text-medium text-xl text-center">Veja os últimos artigos postados</p>
       </div>
 
-      <ArticleGrid />
+      <ArticleGrid articles={articlesData.slice(0, seeMore)} />
 
       <div className="w-full py-5 flex justify-center items-center">
-        <Link
-          to="/articles"
-          className="bg-blue-600 hover:bg-blue-700 py-4 px-5 font-bold text-white rounded-md w-[200px] text-center"
-        >
-          Ver mais
-        </Link>
+        {articlesData.length >= seeMore && (
+          <button
+            className="bg-blue-600 hover:bg-blue-700 py-4 px-5 font-bold text-white rounded-md w-[200px] text-center"
+            onClick={handleSeeMore}
+          >
+            Ver mais
+          </button>
+        )}
       </div>
     </section>
   );
