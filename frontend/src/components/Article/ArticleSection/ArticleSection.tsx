52f0f2af1dc328327a753ce35691db344f8553cb
Delziron Lima <delziron.braz@hotmail.com>
2024-08-15 22:57:49 -0300

    fix(home sections links)

--- frontend/src/components/Article/ArticleSection/ArticleSection.tsx
+++ frontend/src/components/Article/ArticleSection/ArticleSection.tsx
@@ -1,14 +1,10 @@
 import { useEffect, useState } from "react";
 import ArticleGrid from "../ArticleGrid/ArticleGrid";
 import { articles, IArticle } from "../../../utils/article-helper";
+import { Link } from "react-router-dom";
 
 const ArticleSection = () => {
   const [articlesData, setArticles] = useState<IArticle[]>([]);
-  const [seeMore, setSeeMore] = useState<number>(4);
-
-  const handleSeeMore = () => {
-    setSeeMore(seeMore + 2);
-  };
 
   useEffect(() => {
     setArticles(articles);
@@ -21,17 +17,14 @@ const ArticleSection = () => {
         <p className="text-medium text-xl text-center">Veja os últimos artigos postados</p>
       </div>
 
-      <ArticleGrid articles={articlesData.slice(0, seeMore)} />
+      <ArticleGrid articles={articlesData.slice(0, 6)} />
 
       <div className="w-full py-5 flex justify-center items-center">
-        {articlesData.length >= seeMore && (
-          <button
-            className="bg-blue-600 hover:bg-blue-700 py-4 px-5 font-bold text-white rounded-md w-[200px] text-center"
-            onClick={handleSeeMore}
-          >
-            Ver mais
-          </button>
-        )}
+        <Link to="/articles"
+          className="bg-blue-600 hover:bg-blue-700 py-4 px-5 font-bold text-white rounded-md w-[200px] text-center"
+        >
+          Ver mais
+        </Link>
       </div>
     </section>
   );
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,27 +1,27 @@
-import { Link } from 'react-scroll';
 import { LuMenu } from "react-icons/lu";
 import { useState } from 'react';
-import { Link as RouterLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Link as LinkAnimation } from "react-scroll";
 
 const Navbar = () => {
     const [menu, setMenu] = useState(false);
 
     const handleChange = () => {
         setMenu(!menu);
-    }
+    };
 
     return (
         <header>
-            <div className='bg-white z-50 md:w-full md:fixed md:top-0 flex flex-row justify-between p-5 md:px-32 bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
+            <div className='bg-white z-50 md:w-full md:fixed md:top-0 flex flex-row justify-between p-5 md:px-32 shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
                 <div>
-                    <RouterLink to="/" className='font-semibold text-2xl p-1 cursor-pointer text-brightGreen'>Mundo Podcast</RouterLink>
+                    <Link to="/" className='font-semibold text-2xl p-1 cursor-pointer text-brightGreen'>Mundo Podcast</Link>
                 </div>
                 <nav className='hidden md:flex gap-5 font-medium p-1 cursor-pointer'>
-                    <Link to="home" spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>Home</Link>
-                    <RouterLink to="/podcasts" className='hover:text-brightGreen transition-all cursor-pointer'>Podcasts</RouterLink>
-                    <Link to="article" spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>Artigos</Link>
-                    <Link to="tutorial" spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>Tutoriais</Link>
-                    <Link to="about" spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>Sobre</Link>
+                    <Link to="/" className='hover:text-brightGreen transition-all cursor-pointer'>Home</Link>
+                    <Link to="/podcasts" className='hover:text-brightGreen transition-all cursor-pointer'>Podcasts</Link>
+                    <Link to="/articles" className='hover:text-brightGreen transition-all cursor-pointer'>Artigos</Link>
+                    <LinkAnimation to="tutorial" spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>Tutoriais</LinkAnimation>
+                    <LinkAnimation to="about" spy={true} smooth={true} duration={500} className='hover:text-brightGreen transition-all cursor-pointer'>Sobre</LinkAnimation>
                 </nav>
 
                 <div className='flex md:hidden' onClick={handleChange}>
@@ -30,12 +30,12 @@ const Navbar = () => {
                     </div>
                 </div>
 
-                <nav className={`${menu ? "translate-x-0" : "-translate-x-full"} z-20 h-screen bg-white md:hidden flex flex-col absolute bg-white left-0 top-20 font-medium text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-200 ease-in-out`}>
-                    <Link to="home" spy={true} smooth={true} duration={500} onClick={handleChange} className='hover:text-brightGreen transition-all cursor-pointer'>Home</Link>
-                    <RouterLink to="/podcasts" className='hover:text-brightGreen transition-all cursor-pointer' onClick={handleChange}>Podcasts</RouterLink>
-                    <Link to="article" spy={true} smooth={true} duration={500} onClick={handleChange} className='hover:text-brightGreen transition-all cursor-pointer'>Artigos</Link>
-                    <Link to="tutorial" spy={true} smooth={true} duration={500} onClick={handleChange} className='hover:text-brightGreen transition-all cursor-pointer'>Tutoriais</Link>
-                    <Link to="about" spy={true} smooth={true} duration={500} onClick={handleChange} className='hover:text-brightGreen transition-all cursor-pointer'>Sobre</Link>
+                <nav className={`${menu ? "translate-x-0" : "-translate-x-full"} z-20 h-screen bg-white md:hidden flex flex-col absolute left-0 top-20 font-medium text-2xl text-center pt-8 pb-4 gap-8 w-full transition-transform duration-200 ease-in-out`}>
+                    <Link to="/" onClick={handleChange} className='hover:text-brightGreen transition-all cursor-pointer'>Home</Link>
+                    <Link to="/podcasts" className='hover:text-brightGreen transition-all cursor-pointer' onClick={handleChange}>Podcasts</Link>
+                    <Link to="/articles" onClick={handleChange} className='hover:text-brightGreen transition-all cursor-pointer'>Artigos</Link>
+                    <LinkAnimation to="tutorial" spy={true} smooth={true} duration={500} onClick={handleChange} className='hover:text-brightGreen transition-all cursor-pointer'>Tutoriais</LinkAnimation >
+                    <LinkAnimation to="about" spy={true} smooth={true} duration={500} onClick={handleChange} className='hover:text-brightGreen transition-all cursor-pointer'>Sobre</LinkAnimation >
                 </nav>
             </div>
         </header>
