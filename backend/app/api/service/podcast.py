dba9815ef5400454ac2e441eef25000066f1ab01
caio-lelis <cayolelisdf@gmail.com>
2024-08-15 23:20:18 -0300

    feat: adiciona service com novas implementações

--- backend/app/api/service/podcast.py
+++ backend/app/api/service/podcast.py
@@ -4,9 +4,12 @@ from app.api.repository.podcast import read_all_podcast as repo_read_all_podcast
 from app.api.repository.podcast import read_podcast as repo_read_podcast
 from app.api.repository.podcast import update_podcast as repo_update_podcast
 from app.api.schemas.podcast import PodcastCreate
+from app.strategies.validation_strategy import ValidationStrategy
 
-def create_podcast(db: Session, podcast: PodcastCreate):
-    return repo_create_podcast(db, podcast)
+class PodcastService:
+    def create_podcast(self, db: Session, podcast: PodcastCreate, validation_strategy: ValidationStrategy):
+        validation_strategy.validate(podcast)
+        return repo_create_podcast(db, podcast)
 
 def read_all_podcast(db: Session):
     return repo_read_all_podcast(db)
@@ -14,5 +17,6 @@ def read_all_podcast(db: Session):
 def read_podcast(db: Session, podcast_id: int):
     return repo_read_podcast(db, podcast_id)
 
-def update_podcast(db: Session, podcast_id: int, podcast: PodcastCreate):
+def update_podcast(db: Session, podcast_id: int, podcast: PodcastCreate , validation_strategy: ValidationStrategy):
+    validation_strategy.validate(podcast)
     return repo_update_podcast(db, podcast_id, podcast)
